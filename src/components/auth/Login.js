4b06a8a964221dc111621cc5f6ed65fc295607ec
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-09-20 20:11:51 -0500

    Creacion del formulario de login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,46 @@
 import React from 'react'
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+
     return ( 
-        <h1>Login</h1>
+        <div className="form-usuario back-color">
+            <div className="contenedor-form card">
+                <img className="card-image" src="logo-consult.png" alt="Logo"/>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="admin">Administrador</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            id="admin"
+                            name="admin"
+                            placeholder="Nombre del usuario administrador"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Contraseña</label>
+                        <input 
+                            className="form-control"
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu contraseña"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primary btn-block" value="Iniciar sesion" />
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
  
