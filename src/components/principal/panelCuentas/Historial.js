ccb8783367a455c0a1973a90805abfb54dd7cdb7
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-10-06 12:45:26 -0600

    Ocultando los componentes de Historial y Formularios de pagos y cargos cuando no se haya seleccionado un cliente

--- src/components/principal/panelCuentas/Historial.js
+++ src/components/principal/panelCuentas/Historial.js
@@ -1,8 +1,20 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useContext} from 'react';
 import ListadoCargos from './cargos/ListadoCargos';
 import ListadoPagos from './pagos/ListadoPagos';
+import clienteContext from '../../../context/clientes/clienteContext';
 
 const Historial = () => {
+
+    //Obtener el state de clientes
+    const clientesContext = useContext(clienteContext);
+    const { cliente } = clientesContext;
+
+    //Si no hay cliente seleccionado
+    if(!cliente) return null;
+
+    //Array destructuring para extraer los datos del cliente
+    const [clienteActual] = cliente;
+
     return ( 
         <Fragment>
             <h2 className='mb-4 '>Historial</h2>
--- src/components/principal/panelCuentas/PagosCargos.js
+++ src/components/principal/panelCuentas/PagosCargos.js
@@ -1,8 +1,20 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useContext} from 'react'
 import FormCargo from './cargos/FormCargo';
 import FormPago from './pagos/FormPago';
+import clienteContext from '../../../context/clientes/clienteContext';
 
 const PagosCargos = () => {
+    
+    //Obtener el state de clientes
+    const clientesContext = useContext(clienteContext);
+    const { cliente } = clientesContext;
+
+    //Si no hay cliente seleccionado
+    if(!cliente) return null;
+
+    //Array destructuring para extraer los datos del cliente
+    const [clienteActual] = cliente;
+    
     return ( 
         <Fragment>
             <FormCargo />
